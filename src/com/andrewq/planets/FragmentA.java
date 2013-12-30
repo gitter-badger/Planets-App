1a51a018514abd7e354d05f8f574d45bcd8952fc
Andrew Quebe <andrewquebe.14@gmail.com>
2013-12-30 15:23:13 -0800

    Test

--- src/com/andrewq/planets/FragmentA.java
+++ src/com/andrewq/planets/FragmentA.java
@@ -12,6 +12,7 @@ import android.widget.Button;
 public class FragmentA extends Fragment {
 
     Button button;
+    //Hello
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
