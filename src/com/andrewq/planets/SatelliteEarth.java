a7e993adc37669b964b992fffb253803c2faa832
ethanthomas <ethan@Ethans-MacBook-Pro.local>
2014-01-20 21:12:15 -0500

    nav

--- src/com/andrewq/planets/SatelliteEarth.java
+++ src/com/andrewq/planets/SatelliteEarth.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.andrewq.planets;
 
 import android.app.Activity;
@@ -35,3 +36,42 @@ public class SatelliteEarth extends Activity {
         }
 
 }
+=======
+package com.andrewq.planets;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SatelliteEarth extends Activity {
+
+	Button button;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.satellite_earth);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		super.onCreate(savedInstanceState);
+
+		button = (Button) findViewById(R.id.moon_button);
+
+		button.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				String url = "http://space-facts.com/the-moon/";
+				Intent i = new Intent(Intent.ACTION_VIEW);
+				i.setData(Uri.parse(url));
+				startActivity(i);
+
+			}
+		});
+
+	}
+
+}
+>>>>>>> a8d54481e97620109eb48f52aec264f0425c78e4
