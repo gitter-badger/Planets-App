44a837a3b584877b7f3fbfad7001b006724c6b74
ethanthomas <ethan@Ethans-MacBook-Pro.local>
2014-01-20 22:34:05 -0500

    fixed settings back stack

--- src/com/obsidian/planets/Main.java
+++ src/com/obsidian/planets/Main.java
@@ -31,7 +31,6 @@ import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 import android.widget.AdapterView;
@@ -40,7 +39,6 @@ import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 import com.andrewq.planets.*;
 
@@ -92,8 +90,6 @@ public class Main extends SherlockFragmentActivity {
 		}
 
 	}
-
-	// ----------------------------RSS-----------------------
 
 	@Override
 	public boolean onPrepareOptionsMenu(Menu menu) {
