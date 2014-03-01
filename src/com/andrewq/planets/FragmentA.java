bb8dc5a0dbef75d80357f23c32b6116158aa5346
Andrew Quebe <andrewquebe.14@gmail.com>
2014-02-28 21:42:36 -0800

    Added zoomed image to the sun as well as some minor bug fixes

--- src/com/andrewq/planets/FragmentA.java
+++ src/com/andrewq/planets/FragmentA.java
@@ -8,16 +8,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class FragmentA extends Fragment {
 
 	Button button;
+	ImageView imageView;
 
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 
+		// final Context classContext = null;
 		button = (Button) getView().findViewById(R.id.button_1);
+		imageView = (ImageView) getView().findViewById(R.id.sun);
 
 		button.setOnClickListener(new View.OnClickListener() {
 
@@ -28,6 +33,16 @@ public class FragmentA extends Fragment {
 				Intent i = new Intent(Intent.ACTION_VIEW);
 				i.setData(Uri.parse(url));
 				startActivity(i);
+			}
+		});
+
+		imageView.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getActivity(), "NOTE: This is still in Beta.",
+						Toast.LENGTH_LONG).show();
+				startActivity(new Intent(getActivity(), SunImageView.class));
 			}
 		});
 	}
